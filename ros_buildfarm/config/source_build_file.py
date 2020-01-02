74bbcf706561393ade1af9a8dfef99bb051fb524
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-01-02 15:45:18 +0100

    Fix syntax

--- ros_buildfarm/config/source_build_file.py
+++ ros_buildfarm/config/source_build_file.py
@@ -132,7 +132,7 @@ class SourceBuildFile(BuildFile):
             if 'run_only_gpu_tests' in data['tests_require_gpu']:
                 if 'default' in data['tests_require_gpu']['run_only_gpu_tests']:
                     self.run_only_gpu_tests = bool(
-                        data['tests_require_gpu']['run_only_gpu_tests']['default']
+                        data['tests_require_gpu']['run_only_gpu_tests']['default'])
 
         self.collate_test_stats = bool(data.get('collate_test_stats', False))
 
